c768589367ffaef67d63d4684af646c5ee0ff6d0
Joseph Martinsen <joseph@martinsen.com>
2018-02-10 20:31:33 -0600

    Added increment endpoint
    takes in a secret

--- server/increment.php
+++ server/increment.php
@@ -1,17 +1,24 @@
 <?php
-    $debug = false;
-    include('CommonMethods.php');
-    $COMMON = new Common($debug);
+    $secret = (int) $_POST['secret'];
 
-    // Increpent PeopleCounts
-    $sql = "INSERT INTO `josephmart`.`PeopleCounts` (`id`, `time`) VALUES (NULL, CURRENT_TIMESTAMP)";
-    $rs = $COMMON->executeQuery($sql, $_SERVER["SCRIPT_NAME"]);
+    if ($secret == 69) {
+        $debug = false;
+        include('CommonMethods.php');
+        $COMMON = new Common($debug);
 
-    // Respond to post request
-    $data = [
-        "status" => "success",
-        "query" => $sql
-    ];
-    header('Content-Type: application/json');
-    echo json_encode($data);
+        // Increpent PeopleCounts
+        $sql = "INSERT INTO `josephmart`.`PeopleCounts` (`id`, `time`) VALUES (NULL, CURRENT_TIMESTAMP)";
+        $rs = $COMMON->executeQuery($sql, $_SERVER["SCRIPT_NAME"]);
+
+        // Respond to post request
+        $data = [
+            "status" => "success",
+            "query" => $sql
+        ];
+
+        header('Content-Type: application/json');
+        echo json_encode($data);
+    } else {
+        http_response_code(403);
+    }
 ?>
