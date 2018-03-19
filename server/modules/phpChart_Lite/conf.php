bf8b2c82f964b4eda77ddef435dcf21500f96892
Joseph Martinsen <joseph@martinsen.com>
2018-03-18 20:37:31 -0500

    Added ability to use phpchart
    can view at /charts.php

---
+++ server/modules/phpChart_Lite/conf.php
@@ -0,0 +1,12 @@
+<?php
+define('SCRIPTPATH', '/home/ugrads/j/josephmart/web_project/p1/modules/phpChart_Lite/');
+define('DEBUG', false);
+
+
+
+
+
+/******** DO NOT MODIFY ***********/
+require_once('phpChart.php');     
+/**********************************/
+?>
