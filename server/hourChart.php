5e807cb289a95e4170f19e2fd8c70fea80befa62
Joseph Martinsen <joseph@martinsen.com>
2018-03-29 16:18:21 -0500

    Hour chart has working data analysis

--- server/hourChart.php
+++ server/hourChart.php
@@ -71,6 +71,20 @@
 
             $rs = $COMMON->executeQuery($sql, $_SERVER["SCRIPT_NAME"]);
             $result = $rs->fetchAll();
+
+            // Analysis queries
+            $countSql = "SELECT 
+                SUM(going_in) as totalIn, 
+                AVG(going_in) as averageIn, 
+                MIN(going_in) as minIn, 
+                MAX(going_in) as maxIn,
+                SUM(going_out) as totalOut, 
+                AVG(going_out) as averageOut, 
+                MIN(going_out) as minOut, 
+                MAX(going_out) as maxOut
+            FROM (".$sql.") as sub_q";
+            $rs = $COMMON->executeQuery($countSql, $_SERVER["SCRIPT_NAME"]);
+            $resultCounts = $rs->fetchAll()[0];
         ?>
     </head>
     <body>
@@ -103,6 +117,58 @@
         </nav>
         <div class="container">
             <h1>Today Chart</h1>
+            
+            <h3>Entering</h3>
+
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Mean</th>
+                        <th>Median</th>
+                        <th>Mode</th>
+                        <th>Max</th>
+                        <th>Min</th>
+                        <th>Count</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><?php echo $resultCounts["averageIn"] ?></td>
+                        <td>-1</td>
+                        <td>-1</td>
+                        <td><?php echo $resultCounts["maxIn"] ?></td>
+                        <td><?php echo $resultCounts["minIn"] ?></td>
+                        <td><?php echo $resultCounts["totalIn"] ?></td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <h3>Leaving</h3>
+
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Mean</th>
+                        <th>Median</th>
+                        <th>Mode</th>
+                        <th>Max</th>
+                        <th>Min</th>
+                        <th>Count</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><?php echo $resultCounts["averageOut"] ?></td>
+                        <td>-1</td>
+                        <td>-1</td>
+                        <td><?php echo $resultCounts["maxOut"] ?></td>
+                        <td><?php echo $resultCounts["minOut"] ?></td>
+                        <td><?php echo $resultCounts["totalOut"] ?></td>
+                    </tr>
+                </tbody>
+            </table>
+
+
             <div id="chart_div" style="width: 800px; height: 500px;margin: auto;"></div>
             
             <form action="" method="post">
@@ -121,6 +187,7 @@
         <script type="text/javascript">
             // DB data to JS
             var db_data = <?php echo json_encode($result) ?>;
+            var resultCounts = <?php echo json_encode($resultCounts) ?>;
             var graph_data = [['Date Times', 'Entering', 'Exiting']];
             var row = {};
             
@@ -129,7 +196,7 @@
                 row = db_data[i];
                 graph_data.push([moment(row.date).format('hh:mm a'), parseInt(row.going_in, 10), parseInt(row.going_out, 10)]);
             }
-            console.log(graph_data);
+            console.log(resultCounts);
             // Get Current range dates
             var start_date = graph_data[1][0];
             var end_date = graph_data[graph_data.length - 1][0];
