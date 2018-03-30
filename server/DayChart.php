b150bfcd24a831d6532eccf51ec87aa5031c27ce
Joseph Martinsen <joseph@martinsen.com>
2018-03-30 01:44:28 -0500

    Navbar modularized

--- server/DayChart.php
+++ server/DayChart.php
@@ -61,33 +61,7 @@ include('Partials.php');
         ?>
     </head>
     <body>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="#">315 Project 1</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php">Table</a>
-                    </li>
-                    <li class="nav-item dropdown active">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Charts
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="MonthChart.php">Month</a>
-                            <a class="dropdown-item" href="WeekChart.php">Week</a>
-                            <a class="dropdown-item" href="DayChart.php">Day</a>
-                            <a class="dropdown-item" href="HourChart.php">Hour</a>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="Admin.php">Admin</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+        <?php HtmlNavbar('charts'); ?>
         <div class="container">
             <h1>Day Chart</h1>
 
--- server/Partials.php
+++ server/Partials.php
@@ -53,4 +53,58 @@ function HtmlHeader($pageName) {
         <script type="text/javascript" src="./js/charts.js"></script>
 HTML;
 }
+
+//-------------------------------------------------------
+// Name: HtmlNavbar
+// PostCondition: Prints template for site navbar
+//---------------------------------------------------------
+function HtmlNavbar($activePage) {
+
+    $tableActive = '';
+    $chartsActive = '';
+    $adminActive = '';
+
+    switch ($activePage) {
+        case 'table':
+            $tableActive = 'active';
+            break;
+        case 'charts':
+            $chartsActive = 'active';
+            break;
+        case 'admin':
+            $adminActive = 'active';
+            break;
+        default:
+            break;
+    }
+    print <<< HTML
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="index.php">315 Project 1</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item {$tableActive}">
+                    <a class="nav-link" href="index.php">Table</a>
+                </li>
+                <li class="nav-item dropdown  {$chartsActive}">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Charts
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="MonthChart.php">Month</a>
+                        <a class="dropdown-item" href="WeekChart.php">Week</a>
+                        <a class="dropdown-item" href="DayChart.php">Day</a>
+                        <a class="dropdown-item" href="HourChart.php">Hour</a>
+                    </div>
+                </li>
+                <li class="nav-item  {$adminActive}">
+                    <a class="nav-link" href="Admin.php">Admin</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+HTML;
+}
 ?>
