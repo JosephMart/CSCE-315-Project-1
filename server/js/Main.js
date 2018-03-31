c751cfc05dae6cf067fead9d6db92d1ce4fe5711
Joseph Martinsen <joseph@martinsen.com>
2018-03-31 15:13:47 -0500

    Removed unused JS function

--- server/js/Main.js
+++ server/js/Main.js
@@ -55,28 +55,3 @@ function LoadGraph(dbData, formatDate) {
     google.charts.load('current', {'packages':['corechart', 'bar']});
     google.charts.setOnLoadCallback(drawChart);
 }
-
-// https://jonlabelle.com/snippets/view/javascript/calculate-mean-median-mode-and-range-in-javascript
-function mode(numbers) {
-    // as result can be bimodal or multi-modal,
-    // the returned result is provided as an array
-    // mode of [3, 5, 4, 4, 1, 1, 2, 3] = [1, 3, 4]
-    var modes = [], count = [], i, number, maxIndex = 0;
- 
-    for (i = 0; i < numbers.length; i += 1) {
-        number = numbers[i];
-        count[number] = (count[number] || 0) + 1;
-        if (count[number] > maxIndex) {
-            maxIndex = count[number];
-        }
-    }
-
-    for (i in count)
-        if (count.hasOwnProperty(i)) {
-            if (count[i] === maxIndex) {
-                modes.push(Number(i));
-            }
-        }
-
-    return modes;
-}
