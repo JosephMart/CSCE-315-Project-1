05edf42155d80226c59d47a84e3e4ec9d191ca2b
joseph <joseph@martinsen.com>
2018-03-31 16:21:36 -0500

    Added Python Arduino test cases

---
+++ arduino/tests/TestArduino.py
@@ -0,0 +1,30 @@
+import unittest
+import sys
+import asyncio
+
+from pymata_aio.pymata3 import PyMata3
+
+
+class TestApi(unittest.TestCase):
+    '''Test Api Module'''
+
+    def setUp(self):
+        pass
+
+    def testConnection(self):
+        '''
+        Test connection to the board as well as Firmware is loaded
+        https://github.com/MrYsLab/pymata-aio/issues/63#issuecomment-377400310
+        '''
+        try:
+            board = PyMata3()
+        # Board not plugged in - correct Firmware/sketch or not
+        except TypeError:
+            assert False
+        # incorrect Firmware/sketch loaded
+        except asyncio.futures.CancelledError:
+            assert False
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
+
